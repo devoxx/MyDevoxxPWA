c9c458dde46e0b2c40fc7a52bd3c4d06cf62e861
Horacio <horacio.gonzalez@gmail.com>
2016-12-18 22:35:16 +0100

    Initial commit in new repository

---
+++ sw-precache-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+  ],
+  stripPrefix: '',
+  replacePrefix:  '.',
+  navigateFallback: 'index.html',
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+  runtimeCaching: [{
+    urlPattern: /.*/,
+    handler: 'networkOnly'
+  }]
+};
